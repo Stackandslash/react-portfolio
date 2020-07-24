29108ce22a13e2f89cc14139fbad3a299998790b
Haven <stackandslash@gmail.com>
2020-07-23 18:02:14 -0700

    broke out port cards

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -18,14 +18,24 @@ function portfolio(props) {
           <h1 className="header">My Portfolio</h1>
           <hr className="my-4" />
           <CardDeck>
-          {projects.map(project => (
           <ProjectCard
-            name={project.name}
-            image={project.image}
-            link={project.link}
-            description={project.description}
+            name="Employee Cards"
+            image={employeecards}
+            link="https://github.com/Stackandslash/employee-summary/blob/master/README.md"
+            description="This command-line application creates a page of employee cards for your site, based on your specifications."
           />
-          ))}
+          <ProjectCard
+            name="Weather Dashboard"
+            image={weatherdashboard}
+            link="https://stackandslash.github.io/weather-dash/"
+            description="This application is a simple way to find weather conditions around the country."
+          />
+          <ProjectCard
+            name="Employee Cards"
+            image={scheduler}
+            link="https://stackandslash.github.io/day-planner-w3d4/"
+            description="This application is a quick option to make and save notes by the hour."
+          />
           </CardDeck>
         </Jumbotron>
     </div>
